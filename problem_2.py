e87986150199ec4b8d440bf9ce56b5cceaf0ffe0
Farza <farza@visor.gg>
2018-10-29 16:48:09 -0700

    second problem

--- problem_2.py
+++ problem_2.py
@@ -1,5 +1,6 @@
 arr = [1, 2, 3, 4, 5]
 
+# 1
 output = []
 for i in range(0, len(arr)):
     curr = 1
@@ -8,4 +9,12 @@ for i in range(0, len(arr)):
             continue
         curr = curr * arr[j]
     output.append(curr)
-print(output)
+print("1 ", output)
+
+# 2
+output = []
+from functools import reduce
+for i in range(0, len(arr)):
+    list_to_multiply = arr[0:i] + arr[i+1:]
+    output.append(reduce(lambda x, y: x*y, list_to_multiply))
+print("2 ", output)
